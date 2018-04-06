2a21c1994bb74072a8c6810c14ca1f67ea350461
RaduB87 <radub87@gmail.com>
2018-04-06 12:25:58 +0300

    added View List Button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,57 @@
 $(document).ready(function(){
-    // Continue
-});
+    
 
+// GLOBAL APP CONTROLLER
+
+let controller = (function(){
+
+    let setupEventListeners = function(){
+
+        $('#get-hospitals').on('click', function(){
+            
+            $.getJSON('assets/data/hospitals.json', function(data){
+                let output = `<table class="table">`;
+                output += `<thead class="thead-light">
+                <tr>
+                  <th>Hospital Name</th>
+                  <th>City</th>
+                  <th>County</th>
+                  <th>Address</th>
+                  <th>Contact</th>
+                </tr>
+                </thead>
+                <tbody>`;
+                $.each(data, function(key,value){
+            
+                    output += `<tr>`;
+                    output += `<td>${value.name}</td>`;
+                    output += `<td>${value.city.name}</td>`;
+                    output += `<td>${value.county.name}</td>`;
+                    output += `<td>${value.address}</td>`;
+                    output += `<td>${value.phone} <br> <a href="${value.website}">Go to website</a></td>`;
+                    output += `</tr>`;
+            
+                })
+                output += `</tbody>`;
+    
+                $('.hospitals-list').html(output);
+                
+            });
+
+        });
+    
+    };
+
+    return {
+        init: function(){
+            setupEventListeners();
+        }
+    }
+    
+
+}());
+
+controller.init();
+
+});
 
